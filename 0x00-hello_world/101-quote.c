be028b3061cf00d24ae49c2ec1a53161b6898535
Euchri <euchy89@gmail.com>
2023-02-16 13:34:08 -0800

    resolved

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 /**
- * main - prints the string to the standard error
+ * main - prints the exactly the line given to the standard error
  * return: 0 if successful
  */
 int main(void)
 {
-	write(2, "and the piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
