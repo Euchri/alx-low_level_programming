1b20d85cdee4aef5879d95b0700fb2679875c211
Euchri <euchy89@gmail.com>
2023-02-16 12:52:18 -0800

    printing a line o the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <unist.h>
+#include <unistd.h>
 /**
  * main - prints the string to the standard error
  * return: 0 if successful, 1 if any other result
  */
 int main(void)
 {
-	write(2, "and the piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	sizeof ("and that piece of art is useful\" - Dora Korpar, 2015-10-19")):
+	write(2, "and the piece of art is useful\" - Dora Korpar, 2015-10-19\n",);
 	return (1);
 }
