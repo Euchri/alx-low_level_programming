82db17778c42e85ec86c2185a565fb02b12c0e52
Euchri <euchy89@gmail.com>
2023-02-16 14:01:50 -0800

    resolved error

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * Description:main - Prints out a string
- * return: 0 if success
+ *main - Entry point
+ *return: 0 if success
  */
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
