5576929b61f0f79e1398bf83f690297b2c93364d
Euchri <euchy89@gmail.com>
2023-02-16 13:28:52 -0800

    fixed erro

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - prints the string to the standard error
@@ -6,6 +5,6 @@
  */
 int main(void)
 {
-	write(2, "and the piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
-	return(1);
+	write(2, "and the piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
