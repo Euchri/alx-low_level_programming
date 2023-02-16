5000fa6af639975b5ef37cb8d30a192daa6cd7dd
Euchri <euchy89@gmail.com>
2023-02-16 10:05:57 -0800

    C program that prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *main prints out a string to stdout
- *Return:0 if successful
- */
+*main prints out a string to stdout
+*Return:0 if successful
+*/
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return(0);
+   puts("\"Programming is like building a multilingual puzzle");
+   return (0);
 }
