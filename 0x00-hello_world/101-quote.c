3c18c1267b7e122c44645cd0797de9ebdfd5e6b8
Euchri <euchy89@gmail.com>
2023-02-16 13:53:17 -0800

    fixed it

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 /**
  *main - prints the exactly the line given
- *return: 0 if successful
+ *return: 0 if successful, otherwise return 1
  */
 int main(void){
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
