31122e6c6955c02bd6922c5d499400120a87c795
Euchri <euchy89@gmail.com>
2023-02-17 16:44:07 -0800

    printing numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: (0) success
+ */
+int main(void)
+{
+	int i = 1;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
