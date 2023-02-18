cc151e2d49c2932b10d7055d17f7482f920de244
Euchri <euchy89@gmail.com>
2023-02-17 17:13:40 -0800

    adding base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,13 +10,15 @@ int main(void)
 	char ch;
 	char i;
 
+	alp='a';
 	for (i = 0; i < 10; i++)
 	{
 		putchar(i + '0');
 	}
 	for (ch = 10; ch <= 15; ch++)
 	{
-		putchar(ch);
+		putchar(alp);
+		alp++;
 	}
 	putchar('\n');
 	return (0);
