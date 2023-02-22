017f7d5e4c849237087dfb99cfdfd9a76b748e0b
Euchri <euchy89@gmail.com>
2023-02-22 07:48:08 -0800

    program that prints the first 50 Fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	long int x = 0, y = 1, z;
 	int n;
-
+	
 	for (n = 0; n < 50; n++)
 	{
 		z = x + y;
@@ -21,6 +21,5 @@ int main(void)
 		y = z;
 	}
 	putchar('\n');
-
 	return (0);
 }
