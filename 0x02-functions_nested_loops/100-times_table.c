705f05b7f9a2ab0074886922068c29b6b1b68eef
Euchri <euchy89@gmail.com>
2023-02-22 07:02:16 -0800

    function that prints the n times table, starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,50 +4,37 @@
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
  */
+
 void print_times_table(int n)
 {
-	int i, j, k;
-	
-	if (n >= 0 && n <= 15)
+	int result, num2, num;
+
+	if (n <= 15 && n >= 0)
 	{
-		for (i = 0; i <= n; i++)
+		for (num = 0; num <= n; ++num)
 		{
-			for (j = 0; j <= n; j++)
+			_putchar(48);
+			for (num2 = 1; num2 <= n; ++num2)
 			{
-				k = j * i;
-				
-				if (j == 0)
+				_putchar(',');
+				_putchar(' ');
+
+				result = num * num2;
+
+				if (result <= 9)
+					_putchar(' ');
+				if (result <= 99)
+					_putchar(' ');
+
+				if (result >= 100)
 				{
-					_putchar(k + '0');
-				}
-				else if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				}
-				else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-				else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
+					_putchar((result / 100) + 48);
+					_putchar((result / 10) % 10 + 48);
+				} else if (result <= 99 && result >= 10)
+					_putchar((result / 10) + 48);
+				_putchar((result % 10) + 48);
 			}
 			_putchar('\n');
 		}
 	}
 }
-
-
