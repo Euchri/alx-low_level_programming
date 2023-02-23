0c700d5d2f147d057a09c18fa0beba41f69ad796
Euchri <euchy89@gmail.com>
2023-02-23 15:46:33 -0800

    function that prints squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -4,19 +4,16 @@
  * print_square - prints a square
  * @size - size of the square
  */
-
 void print_square(int size)
 {
 	int i, j;
 
 	if (size > 0)
 	{
-		for (i = 1; i <= size; i++)
+		for (j = 0; j < size; j++)
 		{
-			for (j = 1; j <=size; j++)
-			{
+			for (i = 0; i < size; i++)
 				_putchar('#');
-			}
 			_putchar('\n');
 		}
 	}
