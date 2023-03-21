b37a3c454c83ffbbfd1da5fdb6b416d112585b03
Euchri <euchy89@gmail.com>
2023-03-21 10:53:31 -0700

    Write a function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,40 +1,35 @@
 #include <stdlib.h>
 #include <string.h>
-
-typedef struct {
-    char* name;
-    float age;
-    char* owner;
-} dog_t;
-
+/**
+ * new_dog - creates a new dog.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ * Return: struct dog.
+ * if fails, returns NULL.
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog = malloc(sizeof(dog_t));
-
-	if (dog == NULL)
-	{
-		return NULL;
-	}
-
-	dog->name = malloc(strlen(name) + 1);
-
-	if (dog->name == NULL)
-	{
-		free(dog);
-		return NULL;
-	}
-
-	strcpy(dog->name, name);
-	dog->age = age;
-
-	dog->owner = malloc(strlen(owner) + 1);
-
-	if (dog->owner == NULL)
-	{
-		free(dog->name);
-		free(dog);
-		return NULL;
-	}
-	strcpy(dog->owner, owner);
-	return dog;
+dog_t *dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+{
+return (NULL);
+}
+dog->name = malloc(strlen(name) + 1);
+if (dog->name == NULL)
+{
+free(dog);
+return (NULL);
+}
+strcpy(dog->name, name);
+dog->age = age;
+dog->owner = malloc(strlen(owner) + 1);
+if (dog->owner == NULL)
+{
+free(dog->name);
+free(dog);
+return (NULL);
+}
+strcpy(dog->owner, owner);
+return (dog);
 }
