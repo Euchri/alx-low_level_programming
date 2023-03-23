c03cd89f59b98a423b121b0dccd3f368d31c0cc1
Euchri <euchy89@gmail.com>
2023-03-23 11:49:47 -0700

    a program that performs simple operations

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,17 +2,9 @@
 
 /**
  * op_add - adds two integers
- * op-sub_subtracts two integers
- * op_mul - multiply two integers
- * op_div -divides two integers
- * op_mod - calculates the modulus of two integers
  * @a: first integer
  * @b: second integer
  * Return: sum of two integers
- * Return: differences of two integers
- * Return: multiple of two integers
- * Return: quotient of two integers
- * Return: modulo of two integers
  */
 
 int op_add(int a, int b)
@@ -20,15 +12,36 @@ int op_add(int a, int b)
 	return (a + b);
 }
 
+/**
+ * op_sub - subtracts two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: difference of two integers
+ */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
+/**
+ * op_mul - multiply two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: product of two integers
+ */
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
+/**
+ * op_div - divides two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: quotient of two integers
+ */
 
 int op_div(int a, int b)
 {
@@ -39,6 +52,13 @@ int op_div(int a, int b)
 	}
 	return (a / b);
 }
+
+/**
+ * op_mod - calculates modulus two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: modulus of two integers
+ */
 
 int op_mod(int a, int b)
 {
