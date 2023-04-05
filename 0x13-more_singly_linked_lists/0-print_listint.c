2721aa180ae1e818f02bb48d64d2db31e62e12e3
Euchri <euchy89@gmail.com>
2023-04-05 15:35:54 -0700

    resolved

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,9 +9,9 @@ size_t print_listint(const listint_t *h)
 {
 	size_t nodeA = 0;
 
-	while (h->next != 0)
+	while (h != 0)
 	{
-		printf("%d\n", h->n);
+		printf("%d\n", h->next);
 		h = h->next;
 		nodeA++;
 	}
